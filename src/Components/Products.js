61275be3fa0104d058a8e9c5c6d0e9f5eccb84ff
Rafael Krueger <rafaelkrueger565@gmail.com>
2022-12-28 16:09:12 -0300

    feat:filter

--- src/Components/Products.js
+++ src/Components/Products.js
@@ -34,13 +34,17 @@ function Products({ data, setData, cart, setCart, handleSetCart }) {
   return (
     <>
       <div className="poducts-style">
-        {!filterActive ? (
+        {screen < 600 && !filterActive ? (
           <BsFilterCircle
             color="black"
             onClick={() => {
               setFilterActive(!filterActive);
             }}
-            style={{ position: "absolute", top: "130px" }}
+            style={{
+              position: "absolute",
+              top: "130px",
+              display: screen > 600 ? "none" : "block",
+            }}
           />
         ) : (
           <BsFilterCircleFill
@@ -48,180 +52,187 @@ function Products({ data, setData, cart, setCart, handleSetCart }) {
             onClick={() => {
               setFilterActive(!filterActive);
             }}
-            style={{ position: "absolute", top: "130px" }}
+            style={{
+              position: "absolute",
+              top: "130px",
+              display: screen > 600 ? "none" : "block",
+            }}
           />
         )}
 
-        {screen < 600 && filterActive ? (
-          <div className="col" id="filter-product-style">
-            <div className="col" id="filter-product-search">
-              <div className="filter-product-search-input">
-                <h3 className="filter-product-title" style={{ color: "black" }}>
-                  <AiOutlineSearch
-                    className="filter-product-icons"
-                    color="black"
-                  />
-                  Pesquisa
-                </h3>
-                <hr />
-                <input
-                  className="filter-product-input"
-                  type="text"
-                  placeholder="Time, país, liga"
-                  onChange={(e) => {
-                    setSearch(e.target.value);
-                  }}
+        <div
+          className="col"
+          id="filter-product-style"
+          style={{
+            display: screen > 600 || filterActive ? "block" : "none",
+          }}
+        >
+          <div className="col" id="filter-product-search">
+            <div className="filter-product-search-input">
+              <h3 className="filter-product-title" style={{ color: "black" }}>
+                <AiOutlineSearch
+                  className="filter-product-icons"
+                  color="black"
                 />
-              </div>
-              <div className="filter-product-search-input">
-                <h3 className="filter-product-title" style={{ color: "black" }}>
-                  <BiCategoryAlt
-                    className="filter-product-icons"
-                    color="black"
-                  />
-                  Categorias
-                </h3>
-                <hr />
-                <div className="filter-list-categories">
-                  {data == null
-                    ? ""
-                    : data.categorias.map((list) => {
-                        return (
-                          <>
-                            <div class="input-group mb-3">
-                              <div
-                                class="input-group-text"
-                                style={{ width: "100%" }}
-                              >
-                                <input
-                                  class="form-check-input mt-0"
-                                  type="checkbox"
-                                  value={list}
-                                  onChange={(e) => {
-                                    let permission = true;
-                                    let index;
-                                    for (let i = 0; i < filtering.length; i++) {
-                                      if (filtering[i] == e.target.value) {
-                                        permission = false;
-                                        index = i;
-                                      }
-                                    }
-                                    if (permission) {
-                                      setFilter([...filtering, e.target.value]);
-                                    } else {
-                                      filtering.splice(index, 1);
-                                    }
-                                  }}
-                                  aria-label="Checkbox for following text input"
-                                />
-                                <p className="filter-list-categories-element">
-                                  {list}
-                                </p>
-                              </div>
-                            </div>
-                          </>
-                        );
-                      })}
-                </div>
-              </div>
+                Pesquisa
+              </h3>
+              <hr />
+              <input
+                className="filter-product-input"
+                type="text"
+                placeholder="Time, país, liga"
+                onChange={(e) => {
+                  setSearch(e.target.value);
+                }}
+              />
             </div>
-          </div>
-        ) : (
-          ""
-        )}
-        {filterActive ? (
-          ""
-        ) : (
-          <div class="card-deck" style={{ display: "flex", flexWrap: "wrap" }}>
-            {data == null ? (
-              <>
-                <h3>Não há produtos no momento!</h3>
-                <br />
-                <p>Favor retorne mais tarde...</p>
-              </>
-            ) : (
-              data.produto
-
-                .filter((list) => {
-                  if (
-                    filtering.length != 0 ||
-                    (categoria &&
-                      categoria !== "all" &&
-                      categoria != "Categorias")
-                  ) {
-                    return (
-                      filtering == list.category || categoria == list.category
-                    );
-                  } else {
-                    return list;
-                  }
-                })
-                .filter((list) => {
-                  if (search != "") {
-                    if (list.product.includes(search)) return list.product;
-                  } else {
-                    return list;
-                  }
-                })
-                .map((list) => {
-                  return (
-                    <>
-                      <div
-                        className={`card-element-${
-                          window.outerWidth > 600 ? "product" : "responsive"
-                        }`}
-                      >
-                        <Link
-                          style={{ textDecoration: "none", color: "black" }}
-                          to={`/produto/${list._id}/${data._id}`}
-                        >
-                          <img
-                            id="product-image"
-                            src={list.image}
-                            class="card-img-top"
-                            alt="..."
-                          />
-                          <div class="card-body">
-                            <h5 id="card-title-style">{list.produto}</h5>
-                            <p class="card-text">
-                              {list.product.slice(0, 30)}...
-                            </p>
-                            <hr />
-
-                            <div class="card-text" id="card-text-product">
-                              <p className="value-product-card">
-                                R${list.value.toFixed(2)}
-                              </p>
-                              <button
-                                onClick={(e) => {
-                                  e.preventDefault();
-                                  setCart([...cart, list]);
+            <div className="filter-product-search-input">
+              <h3 className="filter-product-title" style={{ color: "black" }}>
+                <BiCategoryAlt className="filter-product-icons" color="black" />
+                Categorias
+              </h3>
+              <hr />
+              <div className="filter-list-categories">
+                {data == null
+                  ? ""
+                  : data.categorias.map((list) => {
+                      return (
+                        <>
+                          <div class="input-group mb-3">
+                            <div
+                              class="input-group-text"
+                              style={{ width: "100%" }}
+                            >
+                              <input
+                                class="form-check-input mt-0"
+                                type="checkbox"
+                                value={list}
+                                onChange={(e) => {
+                                  let permission = true;
+                                  let index;
+                                  for (let i = 0; i < filtering.length; i++) {
+                                    if (filtering[i] == e.target.value) {
+                                      permission = false;
+                                      index = i;
+                                    }
+                                  }
+                                  if (permission) {
+                                    setFilter([...filtering, e.target.value]);
+                                  } else {
+                                    filtering.splice(index, 1);
+                                  }
                                 }}
-                                className="btn btn-success"
-                              >
-                                Adicionar
-                              </button>
-                            </div>
-                            <br />
-                            <div className="card-category-text">
-                              <p className="card-category-text-p">
-                                {" "}
-                                <BiCategoryAlt
-                                  color="black"
-                                  style={{ marginRight: "5%" }}
-                                />
-                                {list.category}
+                                aria-label="Checkbox for following text input"
+                              />
+                              <p className="filter-list-categories-element">
+                                {list}
                               </p>
                             </div>
                           </div>
-                        </Link>
-                      </div>
-                    </>
-                  );
-                })
-            )}
+                        </>
+                      );
+                    })}
+              </div>
+            </div>
           </div>
-        )}
+        </div>
+
+        <div
+          class="card-deck"
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            display: screen > 600 || !filterActive ? "flex" : "none",
+          }}
+        >
+          {data == null ? (
+            <>
+              <h3>Não há produtos no momento!</h3>
+              <br />
+              <p>Favor retorne mais tarde...</p>
+            </>
+          ) : (
+            data.produto
+
+              .filter((list) => {
+                if (
+                  filtering.length != 0 ||
+                  (categoria &&
+                    categoria !== "all" &&
+                    categoria != "Categorias")
+                ) {
+                  return (
+                    filtering == list.category || categoria == list.category
+                  );
+                } else {
+                  return list;
+                }
+              })
+              .filter((list) => {
+                if (search != "") {
+                  if (list.product.includes(search)) return list.product;
+                } else {
+                  return list;
+                }
+              })
+              .map((list) => {
+                return (
+                  <>
+                    <div
+                      className={`card-element-${
+                        window.outerWidth > 600 ? "product" : "responsive"
+                      }`}
+                    >
+                      <Link
+                        style={{ textDecoration: "none", color: "black" }}
+                        to={`/produto/${list._id}/${data._id}`}
+                      >
+                        <img
+                          id="product-image"
+                          src={list.image}
+                          class="card-img-top"
+                          alt="..."
+                        />
+                        <div class="card-body">
+                          <h5 id="card-title-style">{list.produto}</h5>
+                          <p class="card-text">
+                            {list.product.slice(0, 30)}...
+                          </p>
+                          <hr />
+
+                          <div class="card-text" id="card-text-product">
+                            <p className="value-product-card">
+                              R${list.value.toFixed(2)}
+                            </p>
+                            <button
+                              onClick={(e) => {
+                                e.preventDefault();
+                                setCart([...cart, list]);
+                              }}
+                              className="btn btn-success"
+                            >
+                              Adicionar
+                            </button>
+                          </div>
+                          <br />
+                          <div className="card-category-text">
+                            <p className="card-category-text-p">
+                              {" "}
+                              <BiCategoryAlt
+                                color="black"
+                                style={{ marginRight: "5%" }}
+                              />
+                              {list.category}
+                            </p>
+                          </div>
+                        </div>
+                      </Link>
+                    </div>
+                  </>
+                );
+              })
+          )}
+        </div>
       </div>
     </>
   );
